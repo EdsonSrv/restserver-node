7399dd27e3f915e2ac46079a34610113ca9dc210
Edson Srvn <edson@ciencias.unam.mx>
2021-11-17 13:28:33 -0600

    Validate all fields

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
+const { validarCampos } = require('../middlewares/validar-campos')
 
 const { usuariosGET, usuariosPost, usuariosPut, usuariosDelete } = require('../controllers/usuarios')
 
@@ -8,7 +9,11 @@ const router = Router()
 router.get('/', usuariosGET)
 
 router.post('/', [
-    check('correo', 'El correo no es válido').isEmail()
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password debe ser mayor a 6 caracteres').isLength({ min: 6 }),
+    check('correo', 'El correo no es válido').isEmail(),
+    check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    validarCampos
 ], usuariosPost)
 
 router.put('/:id', usuariosPut)
