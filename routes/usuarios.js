838a3cd3c29fc38b5f548496b9d8003de7753f1f
Edson Srvn <edson@ciencias.unam.mx>
2021-11-17 13:52:34 -0600

    Adding role to model and DB

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
 const { validarCampos } = require('../middlewares/validar-campos')
+const Role = require('../models/role')
 
 const { usuariosGET, usuariosPost, usuariosPut, usuariosDelete } = require('../controllers/usuarios')
 
@@ -12,7 +13,11 @@ router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe ser mayor a 6 caracteres').isLength({ min: 6 }),
     check('correo', 'El correo no es válido').isEmail(),
-    check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // check('rol', 'No es un rol válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom( async(rol = '') => {
+        const existeRol = await Role.findOne({ rol })
+        if( !existeRol ) throw new Error(`El rol ${ rol } no está registrado en la BD`)
+    }),
     validarCampos
 ], usuariosPost)
 
