e47010f5eb4c100bde9672a14257e2ca0c63f3bb
Edson Srvn <edson@ciencias.unam.mx>
2021-11-23 20:56:59 -0600

    Custom validation, emailExiste

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,10 +1,18 @@
 const Role = require('../models/role')
+const Usuario = require('../models/usuario')
 
 const esRoleValido = async(rol = '') => {
     const existeRol = await Role.findOne({ rol })
     if( !existeRol ) throw new Error(`El rol ${ rol } no está registrado en la BD`)
 }
 
+const emailExiste = async(correo = '') => {
+    //Validar correo
+    const existeEmail = await Usuario.findOne({correo})
+    if(existeEmail) throw new Error('Ese correo ya está registrado')
+}
+
 module.exports = {
-    esRoleValido
+    esRoleValido,
+    emailExiste
 }
