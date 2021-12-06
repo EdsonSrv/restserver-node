a0230e0f82a3c21333793eb7aacf2c3ebd6a62de
Edson Srvn <edson@ciencias.unam.mx>
2021-12-06 17:08:22 -0600

    User login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,42 @@
 const { response, request } = require('express')
+const bcryptjs = require('bcryptjs')
 
-const login = (req, res = response) => {
-    res.json({
-        msg: 'Login Ok'
+const Usuario = require('../models/usuario')
+
+const login = async(req, res = response) => {
+  const { correo, password } = req.body
+
+  try {
+
+    //Verificar si el correo existe
+    const usuario = await Usuario.findOne({ correo })
+    if(!usuario) return res.status(400).json({
+      msg: 'Usuario / Password no son correctos - correo'
     })
+    
+    //Validar estdo del usuario
+    if(!usuario.estado) return res.status(400).json({
+      msg: 'Usuario / Password no son correctos - estado: false'
+    })
+
+    //Valida contraseña
+    const validaPasword = bcryptjs.compareSync(password, usuario.password)
+    if(!validaPasword) return res.status(400).json({
+      msg: 'Usuario / Password no son correctos - password'
+    })
+
+    res.json({
+      msg: 'Login Ok'
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      msg: 'Contacte con el administrador'
+    })
+  }
+
 }
 
 module.exports = {
-    login
+  login
 }
--- routes/auth.js
+++ routes/auth.js
@@ -8,9 +8,9 @@ const { login } = require('../controllers/auth')
 const router = Router()
 
 router.post('/login', [
-    check('correo', 'El correo es obligatorio').isEmail(),
-    check('password', 'La contraseña es obligatoria').not().isEmpty(),
-    validarCampos
+  check('correo', 'El correo es obligatorio').isEmail(),
+  check('password', 'La contraseña es obligatoria').not().isEmpty(),
+  validarCampos
 ], login)
 
 module.exports = router
