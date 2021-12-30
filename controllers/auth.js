1069bf3c0f5f7ac8396ecac58d22476e4b1bc1e6
Edson Srvn <edson@ciencias.unam.mx>
2021-12-30 14:09:52 -0600

    creategoogle user

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcryptjs = require('bcryptjs')
 const Usuario = require('../models/usuario')
 const { generaJWT } = require('../helpers/generar-jwt')
 const { googleVerify } = require('../helpers/google-verify')
+const { DefaultTransporter } = require('google-auth-library')
 
 const login = async(req, res = response) => {
   const { correo, password } = req.body
@@ -44,20 +45,42 @@ const login = async(req, res = response) => {
 
 const googleSignIn = async( req, res = response) => {
   const { id_token } = req.body
-
   try {
-
     const { nombre, img, correo } = await googleVerify( id_token )
+    let usuario = await Usuario.findOne({ correo })
+
+    
+    if( !usuario ) {
+      //Hay que crearlo
+      const data = {
+        nombre,
+        correo,
+        rol: 'USER_ROLE',
+        password: ':P',
+        img,
+        google: true
+      }
+      
+      usuario = new Usuario( data )
+      await usuario.save()
+    }
+
+    //Si el usuario en DB
+    if( !usuario.estado ) return res.status(401).json({
+      msg: 'Hable con el administrador, usuario bloqueado'
+    })
+
+    //Generar el JWT
+    const token = await generaJWT( usuario.id )
 
     res.json({
-      msg: 'Todo bien',
-      id_token
+      usuario,
+      token
     })
     
   } catch (error) {
-    json.status(400).json({
-      ok:false,
-      msg: 'El Token no se pudo verificar'
+    res.status(400).json({
+      msg: 'El Token de Google no se pudo verificar'
     })
   }
 
