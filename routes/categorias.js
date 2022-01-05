592e1f79c903eb5496257fe0ccedcd272c021a08
Edson Srvn <edson@ciencias.unam.mx>
2022-01-04 19:12:17 -0600

    create a category

--- routes/categorias.js
+++ routes/categorias.js
@@ -1,7 +1,8 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
+const { crearCategoria } = require('../controllers/categorias')
 
-const { validarCampos } = require('../middlewares/validar-campos')
+const { validarJWT, validarCampos } = require('../middlewares')
 
 const router = Router()
 
@@ -20,9 +21,11 @@ router.get('/:id', (req, res) => {
 })
 
 // Crear categoria - privado - cualquier persona con un token válido
-router.post('/', (req, res) => {
-  res.json('post')
-})
+router.post('/', [ 
+  validarJWT,
+  check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+  validarCampos
+ ], crearCategoria )
 
 // Actualizar - privado - cualquiera con token válido
 router.put('/:id', (req, res) => {
