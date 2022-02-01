00808913cefe131850b95619266cd278b31aff27
Edson Srvn <edson@ciencias.unam.mx>
2022-01-31 19:25:58 -0600

    Search method

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -5,7 +5,7 @@ const { Usuario, Categoria, Producto } = require('../models')
 
 const coleccionesPermitidas = [
   'usuarios',
-  'categoria',
+  'categorias',
   'productos',
   'roles'
 ]
@@ -30,6 +30,42 @@ const buscarUsuarios = async( termino = '', res = response ) => {
   })
 }
 
+const buscarCategorias = async( termino = '', res = response ) => {
+
+  const esMongoID = ObjectId.isValid( termino )
+  if( esMongoID ) {
+    const categoria = await Categoria.findById(termino);
+    return res.json({
+      results: (categoria) ? [ categoria ]: []
+    })
+  }
+
+  const regex = new RegExp( termino, 'i' )
+  const categorias = await Categoria.find({ nombre: regex, estado: true})
+  res.json({
+    results: categorias
+  })
+}
+
+const buscarProducto = async( termino = '', res = response ) => {
+
+  const esMongoID = ObjectId.isValid( termino )
+  if( esMongoID ) {
+    const producto = await Producto.findById(termino)
+                      .populate('categoria', 'nombre')
+    return res.json({
+      results: (producto) ? [ producto ]: []
+    })
+  }
+
+  const regex = new RegExp( termino, 'i' )
+  const productos = await Producto.find({ nombre: regex, estado: true})
+                      .populate('categoria', 'nombre');
+  res.json({
+    results: productos
+  })
+}
+
 const buscar = ( req, res = response ) => {
 
   const { coleccion, termino } = req.params
@@ -43,9 +79,11 @@ const buscar = ( req, res = response ) => {
     case 'usuarios':
       buscarUsuarios( termino, res )
     break;
-    case 'categoria':
+    case 'categorias':
+      buscarCategorias( termino, res )
       break;
     case 'productos':
+      buscarProducto( termino, res )
       break;
 
     default: res.status(500).json({
